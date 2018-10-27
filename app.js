01238e27ec9ef8521c65577f4dba9ffde96e3ef8
Dhruv Patel <39440204+Dpatel0510@users.noreply.github.com>
2018-10-27 14:43:35 -0500

    redis

--- app.js
+++ app.js
@@ -17,4 +17,12 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
+let
+    redis = require('redis'),
+    /* Values are hard-coded for this example, it's usually best to bring these in via file or environment variable for production */
+    client = redis.createClient({
+        port: 16231,               // replace with your port
+        host: 'redis-16231.c85.us-east-1-2.ec2.cloud.redislabs.com',        // replace with your hostanme or IP address
+        password: 'bi4Fxi9q3C9nQor7LNhBnIBK61TJn2KU',    // replace with your password
+    });
 module.exports = app;
