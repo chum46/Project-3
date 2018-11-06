3c7aa5ae6b38597eb6e20b39a536f87013e518bf
Dhruv Patel <39440204+Dpatel0510@users.noreply.github.com>
2018-11-05 19:28:38 -0600

    freeEvents Calls

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var mongoose = require('mongoose');
+var MOngooseCashe = require('mongoose-redis');
+
+var cache = MongooseCashe(mongoose, { port: 6379, host: 'localhost' })
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
---
+++ client/src/pages/activity/Activitypage.js
@@ -0,0 +1,50 @@
+var express = require("express");
+var mongojs = require("mongojs");
+
+var app = express();
+
+var databaseUrl = "freeEvents";
+var collections = ["events"];
+
+var db = mongojs(databaseUrl, collections);
+
+db.on("error", function (error) {
+    console.log("Database Error:", error);
+});
+
+function shuffle(array) {
+    var currentIndex = array.length, temporaryValue, randomIndex;
+
+    while (0 !== currentIndex) {
+
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+    }
+
+    return array;
+}
+
+app.get("/all", function (req, res) {
+    db.events.find({}, function (err, found) {
+        if (err) {
+            console.log(err);
+        }
+        else {
+            const dataArray = found;
+            const newDataArray = [];
+            shuffle(dataArray)
+            for (var i = 0; i < 5; i++) {
+                newDataArray.push(dataArray[i])
+            }
+            res.json(newDataArray);
+        }
+    });
+});
+
+app.listen(3000, function () {
+    console.log("App running on port 3000!");
+});
