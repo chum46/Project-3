0919c50da53ff2641881c8ba428604a45bd7ae01
routhy46 <37312600+chum46@users.noreply.github.com>
2018-11-08 12:47:35 -0600

    rebase try for postActions 2

--- client/src/actions/postActions.js
+++ client/src/actions/postActions.js
@@ -64,7 +64,7 @@ export const createPost = (postData) => dispatch => {
 export const findEvents = (startDate, endDate, type, minPrice, maxPrice) => dispatch => {
     console.log('findEvents action called');
     console.log(startDate + " " + endDate);
-    const ticketmasterAPI = '';
+    const ticketmasterAPI = '#';
     return axios.get(`https://app.ticketmaster.com/discovery/v2/events.json?city=Chicago&apikey=${ticketmasterAPI}&startDateTime=${startDate}T05:30:00Z&endDateTime=${endDate}T04:30:00Z&size=100`)
         .then(response => dispatch({
             type: FETCH_EVENTS,
@@ -75,7 +75,7 @@ export const findEvents = (startDate, endDate, type, minPrice, maxPrice) => disp
 export const findFood = (startDate, endDate, cuisinesAPI, minPrice, maxPrice) => dispatch => {
     // console.log('findEvents action called');
     // console.log(startDate + " " + endDate);
-    const zomatoAPI = '';
+    const zomatoAPI = '#';
     return axios.get(`https://developers.zomato.com/api/v2.1/search?entity_id=292&entity_type=city&cuisines=${cuisinesAPI}`, {
         headers: {
             "user-key": zomatoAPI
