df41f5c4f23530322246e4bb60d494275c14e92b
routhy46 <37312600+chum46@users.noreply.github.com>
2018-11-07 19:48:05 -0600

    added reducers and routing

--- client/src/pages/event/Eventpage.js
+++ client/src/pages/event/Eventpage.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react'
 // import Posts from '../../components/Posts';
 import Eventsearch from '../../components/Eventsearch';
+import Events from '../../components/Events';
 
 class Eventpage extends Component {
     render() {
         return (
             <div>
               <Eventsearch />
+              <hr />
+              <Events />
             </div>
         );
       }
