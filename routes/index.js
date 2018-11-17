fd3ce201c284f17bcd2bae9ce927df99c89a44e8
routhy46 <37312600+chum46@users.noreply.github.com>
2018-11-16 23:31:14 -0600

    express mongoose full backend functionality and routing and connection to frontend

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,15 @@
-var express = require('express');
-var router = express.Router();
+const path = require("path");
+const router = require("express").Router();
+const apiRoutes = require("./api");
+const activityRoutes = require("./activities");
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+// API Routes
+router.use("/api", apiRoutes);
+router.use("/activities", activityRoutes);
+
+// If no API routes are hit, send the React app
+// router.use(function(req, res) {
+//   res.sendFile(path.join(__dirname, "../client/build/index.html"));
+// });
 
 module.exports = router;
